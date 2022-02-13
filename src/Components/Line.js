7bfa2a79497a6a45a4f62858d2b04e58da006708
Ryan Anderson <sonorousduck@gmail.com>
2022-02-12 23:23:27 -0700

    Home screen has a progress bar now, general layout (probably) of what the Day will look like (thogh I need to put weight at the top, or bottom right) and going well

---
+++ src/Components/Line.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Line = () => {
+    return (
+        <View style={{
+            height: 1,
+            backgroundColor: 'rgba(255, 255, 255 ,0.3)',
+            alignSelf: 'stretch'
+        }} />
+    )
+};
+
+
+export default Line;
